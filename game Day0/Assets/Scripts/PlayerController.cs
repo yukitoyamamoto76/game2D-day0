d8cc896766df663d4e38f6abbaf38a85859abf77
yamamoto <yoyoabab762@gmail.com>
2020-06-07 20:16:39 +0900

    make games

---
+++ game Day0/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private int count;
+    public Text CountText;
+    public Text WinText;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        WinText.text = "";
+        SetCount();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);      
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("PickUp"))
+        {
+            collision.gameObject.SetActive(false);
+            count = count + 1;
+            SetCount();
+        }
+    }
+    void SetCount()
+    {
+        if (count >= 12)
+        {
+            WinText.text = "You Win!";
+        }
+    }
+}
